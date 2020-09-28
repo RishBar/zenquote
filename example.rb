2f4151cb8675bb43fee6c69a4ecca496d73112f5
Rishav Baral <rishavbaral6@gmail.com>
2020-09-28 06:39:32 -0700

    use nokogiri to parse goodreads and find a quote about mindfulness

--- example.rb
+++ example.rb
@@ -1,5 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
 
-parsed_data = Nokogiri::HTML.parse(open('https://www.goodreads.com/quotes/tag/mindfulness?page=1')).title
-puts parsed_data
+
+
+document = Nokogiri::HTML.parse(open("https://www.goodreads.com/quotes/tag/mindfulness?page=#{rand(87)}"))
+
+document.css('script').remove
+
+# class_name = "quoteText"
+
+# parsed_data = document.xpath("//*[@class=\"#{class_name}\"]")
+# puts parsed_data
+
+
+quotes = document.css("div[class=quoteText]")
+
+rand_quote_num = rand(quotes.length)
+
+one_quote = quotes[rand_quote_num]
+
+
+puts one_quote
