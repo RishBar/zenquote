9470e17d74867e1d930facf6586c397bb5535723
Rishav Baral <rishavbaral6@gmail.com>
2020-09-28 07:10:49 -0700

    ...

--- example.rb
+++ example.rb
@@ -3,21 +3,13 @@ require 'open-uri'
 
 
 
+
 document = Nokogiri::HTML.parse(open("https://www.goodreads.com/quotes/tag/mindfulness?page=#{rand(87)}"))
 
 document.css('script').remove
 
-# class_name = "quoteText"
-
-# parsed_data = document.xpath("//*[@class=\"#{class_name}\"]")
-# puts parsed_data
-
-
 quotes = document.css("div[class=quoteText]")
 
-rand_quote_num = rand(quotes.length)
+one_quote = quotes[rand(quotes.length)]
 
-one_quote = quotes[rand_quote_num]
-
-
-puts one_quote
+puts one_quote.text
